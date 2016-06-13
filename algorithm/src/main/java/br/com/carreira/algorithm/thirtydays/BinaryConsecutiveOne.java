c6880b5328baa6d87de707bfa9465bc262f0937f
Paulo Carreira <carreira_p@yahoo.com.br>
2016-06-13 12:53:02 -0300

    Binary

--- algorithm/src/main/java/br/com/carreira/algorithm/thirtydays/BinaryConsecutiveOne.java
+++ algorithm/src/main/java/br/com/carreira/algorithm/thirtydays/BinaryConsecutiveOne.java
@@ -1,24 +1,44 @@
 package br.com.carreira.algorithm.thirtydays;
 
 import java.util.Scanner;
-
+//65535  16
 public class BinaryConsecutiveOne {
 	
 	public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        String s = "";
+        //Scanner in = new Scanner(System.in);
+        int n = 44535;
+        StringBuilder s = new StringBuilder("");
         while (true) {
             int r = n % 2;
-            s = s + r;
+            s.append(r);
               
-            if (n/2 == 1 || n%2 == 0) {
-                s = s + (n/2);
+            if (n/2 == 1 ) {
+                s.append(n/2);
                 break;
             }
              n = n / 2 ;
         }
-        System.out.println(s);
+        String bi = s.reverse().toString();
+        System.out.println(bi);
+        int account = 0;
+        int total = 0;
+        for (int i=0; i < bi.length(); i++) {
+        	if (bi.charAt(i) == '1') {
+        		account = account + 1;
+        	} 
+        	
+        	if (bi.charAt(i) == '0') {
+        		account = 0;
+        	}
+        	
+        	if (account > total || total == 0) {
+        			total = account;
+        	}
+        	
+        	
+        }
+        System.out.println(total);
     }
+	
 
 }
